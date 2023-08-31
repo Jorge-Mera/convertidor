f7d303b08c65b2604f0b42bec6d5e55efbc22f02
Jorge-Mera <natemera@gmail.com>
2023-08-30 23:31:32 -0400

    subiendo primera pre-entrega

---
+++ main.js
@@ -0,0 +1,14 @@
+ 
+
+let dolar = 854;
+
+do{
+    numero = prompt("introduce un numero a convertir de dolares a CLP");
+    console.log(numero*dolar);
+}while (parseInt(numero));
+
+if(numero=prompt("")){
+    alert("debe introducir un numero");
+if(numero===0){
+    console.log("es un resultado logico ¿no? es 0")
+}};
