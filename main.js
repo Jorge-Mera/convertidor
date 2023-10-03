3340f5d26323dedab85c20a65eb651de4b8026aa
Jorge-Mera <natemera@gmail.com>
2023-10-02 22:31:06 -0300

    tercera pre-entrega

--- main.js
+++ main.js
@@ -1,59 +1,31 @@
 
-//objectos
-let paises = [
-  {nombre: "Argentina", precio: 0.40},
-  {nombre: "Mexico", precio: 0.019},
-  {nombre: "Peru", precio: 238},
-  {nombre: "Estados Unidos", precio: 887},
-];
 
-let pregunta = prompt("¿Desea convertir CLP a algún país?")
+function calcular()
+{
 
-while(pregunta != "si" && pregunta != "no"){
-  alert("por favor introducta una de las repuesta SI o NO");
-  pregunta = prompt("¿desea convertir dinero de CLP a un pais?");
-}
+let moneda = document.getElementById("moneda").value;
+let monto = document.getElementById("monto").value;
+let argentina = 0.40;
+let mexico = 0.019;
+let peru = 238;
+let estadosUnidos = 887;
+let resultado = 0;
 
 
-if(pregunta == "si"){
-  alert("esta es la lista de paises a convertir");
-  //funcion de orden superior
-  let todosLosPaises = paises.map(
-    (pais) => pais.nombre + " " + "$" + pais.precio);
-  //array
-    alert(todosLosPaises.join(" - "))
-}else if (pregunta == "no"){
-  alert("gracias");
-}
-
-let pregunta2 = prompt("¿que pais desea seleccionar?");
-
-function conversion() { //funcion
-    let numero = prompt("introduce un numero a convertir de dolares a CLP");
-
-    //condicionales
-    if (numero == 0) {
-      console.log("es un resultado logico ¿no?");
-    }
-    if (numero == "") {
-        alert("introduce un dato");
-      }
-    else {
-
-    //ciclo
-      do {
-        console.log("La conversión es $" + numero);
-      } while (numero == parseInt(Number));
-    }
+  if(moneda=="a")
+  {
+      resultado = monto * argentina;
+  }else if(moneda =="b")
+  {
+      resultado = monto * mexico;
+  }else if(moneda=="c")
+  {
+      resultado=monto*peru;
+  }else if(moneda=="d")
+  {
+      resultado=monto*estadosUnidos;
   };
 
-conversion();
 
-
-
-
-
-
-
-
-
+  document.getElementById("resultado").innerHTML="Resultado: $"+resultado.toFixed(2);
+};
